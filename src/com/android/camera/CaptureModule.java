7ac2b749f25077fe8b05cf1406178cb51d09ff8e
Paul Rohde <codelogic@google.com>
2014-12-05 12:17:15 -0800

    Drop new focus indicator into Camera2.
    * Create a new custom focus view that interacts with physical lens diopter changes.
    * Replace all occurances of the old focus indicator with the new one.
    
    Change-Id: Ia02646ce4d1eb059ecb8a1dfccc15dfc9c167e1b

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -104,6 +104,7 @@ import com.android.camera.imageprocessor.filter.SharpshooterFilter;
 import com.android.camera.imageprocessor.filter.StillmoreFilter;
 import com.android.camera.imageprocessor.filter.UbifocusFilter;
 import com.android.camera.ui.CountDownView;
+import com.android.camera.ui.focus.FocusRing;
 import com.android.camera.ui.ModuleSwitcher;
 import com.android.camera.ui.RotateTextToast;
 import com.android.camera.ui.TrackingFocusRenderer;
@@ -1960,7 +1961,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                 mActivity.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        mUI.clearFocus();
+                        mUI.getFocusRing().stopFocusAnimations();
                     }
                 });
             }
@@ -2823,10 +2824,10 @@ public class CaptureModule implements CameraModule, PhotoController,
         int[] newXY = {x, y};
         if (mUI.isOverControlRegion(newXY)) return;
         if (!mUI.isOverSurfaceView(newXY)) return;
-        mUI.setFocusPosition(x, y);
+        mUI.getFocusRing().startActiveFocus();
+        mUI.getFocusRing().setFocusLocation(x, y);
         x = newXY[0];
         y = newXY[1];
-        mUI.onFocusStarted();
         if (isBackCamera()) {
             switch (getCameraMode()) {
                 case DUAL_MODE:
@@ -3252,7 +3253,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             }
 
             requestAudioFocus();
-            mUI.clearFocus();
+            mUI.getFocusRing().stopFocusAnimations();
             mUI.hideUIwhileRecording();
             mCameraHandler.removeMessages(CANCEL_TOUCH_FOCUS, mCameraId[cameraId]);
             mState[cameraId] = STATE_PREVIEW;
@@ -3321,7 +3322,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                                         + e.getMessage());
                             e.printStackTrace();
                         }
-                        mUI.clearFocus();
+                        mUI.getFocusRing().stopFocusAnimations();
                         mUI.resetPauseButton();
                         mRecordingTotalTime = 0L;
                         mRecordingStartTime = SystemClock.uptimeMillis();
@@ -3355,7 +3356,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                         } catch (IllegalStateException e) {
                             e.printStackTrace();
                         }
-                        mUI.clearFocus();
+                        mUI.getFocusRing().stopFocusAnimations();
                         mUI.resetPauseButton();
                         mRecordingTotalTime = 0L;
                         mRecordingStartTime = SystemClock.uptimeMillis();
